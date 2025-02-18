a24ed8c12f10a3b26453f130548a1d55695a53ba
KuzminaKV <karina_zubovo@mail.ru>
2025-02-18 19:14:13 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "utils.cpp"
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << func_sum(a, b);
+    std::cout << func_razn(a, b);
+}
---
+++ src/utils.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int func_sum(int a, int b)
+{
+    return a+b;
+}
+
+int func_razn(int a, int b)
+{
+    return a-b;
+}
